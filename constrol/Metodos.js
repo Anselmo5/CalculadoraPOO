492f4609f88262293c4fe8773f88068ecdab966d
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2023-03-28 09:23:23 -0300

    Update Metodos.js

--- constrol/Metodos.js
+++ constrol/Metodos.js
@@ -10,7 +10,17 @@ class calcula {
         const value = document.getElementById("r").value = this.numero / this.valor
         alert('Seu calculo foi realizado')
     }
-        
+    
+    
+    
+limpeza(){
+    this.numero = Number(this.numero)
+    this.valor = Number(this.valor)
+    this.numero = ("")
+    this.valor = ("")
+    alert("Tudo foi limpo")
+}
+
 }
 
 function enviar(){
@@ -20,3 +30,13 @@ function enviar(){
     console.log("enviado");
     calculo.Cacular()
 } 
+
+
+function limpar(){
+    let x = document.getElementById("a").value
+    let y = document.getElementById("b").value
+    const limpeza = new limpar(x,y)
+    console.log("Limpo");
+    x = (" ")
+    y = (" ")
+}
